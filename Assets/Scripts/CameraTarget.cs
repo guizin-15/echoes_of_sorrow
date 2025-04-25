97d105f9ade09127fcdc6d9b94a9cd390ef38a39
guizin-15 <luigiquinze@gmail.com>
2025-04-24 22:05:20 -0300

    Camera fixed e Parallax menor

---
+++ Assets/Scripts/CameraTarget.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class CameraTarget : MonoBehaviour
+{
+    public Transform player;
+    public float followSpeed = 5f;
+
+    void Update()
+    {
+        if (player == null) return;
+
+        Vector3 targetPos = new Vector3(player.position.x, player.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, targetPos, followSpeed * Time.deltaTime);
+    }
+}
