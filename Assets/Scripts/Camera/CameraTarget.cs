b6138c0fac05ee064b34914f16b3f476bf88dfcb
ribollequis87 <gustavocr2@al.insper.edu.br>
2025-05-12 16:13:09 -0300

    feat: vida com corações | Silvane mais detalhada | câmera melhor posicionada

--- Assets/Scripts/Camera/CameraTarget.cs
+++ Assets/Scripts/Camera/CameraTarget.cs
@@ -9,7 +9,7 @@ public class CameraTarget : MonoBehaviour
     {
         if (player == null) return;
 
-        Vector3 targetPos = new Vector3(player.position.x, player.position.y + 1.7f, transform.position.z);
+        Vector3 targetPos = new Vector3(player.position.x, player.position.y + 2.25f, transform.position.z);
         transform.position = Vector3.Lerp(transform.position, targetPos, followSpeed * Time.deltaTime);
     }
 }
